eb6c4a98a9f0704c444dd3c9d8597926397b1f21
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-04-16 01:20:57 -0400

    Implement a custom bind polyfill

---
+++ scripts/bindPolyfill.js
@@ -0,0 +1,28 @@
+
+
+let person1 = {
+  firstName: "John",
+  lastName: "Doe"
+}
+
+function buidProfile(someArgument, ...moreArguments) {
+  return `${this.firstName} ${this.lastName} ${someArgument} ${moreArguments.join(" ")}`;
+}
+
+let firstBind = buidProfile.bind(person1, "helloFirst", "helloSecond", "helloThird");
+console.log(firstBind());
+
+Function.prototype.myBind = function(context, ...bindArgs) {
+// Need 3 things:
+// 1. The function to be bound. BuildProfile in our case.
+// 2. The context to be used for the function example person1.
+// 3. Any arguments to be passed to the function. customBind in this case.
+//   return (...args) => {
+//     this.apply(context, bindArgs.concat(args));
+//   }
+// }
+  return (...callArgs) => {
+    return this.apply(context, bindArgs.concat(callArgs));
+  }
+}
+
